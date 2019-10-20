698c3ccacc1547a735c78f72fbaeb1d856505b70
e <livisun.well@gmail.com>
2019-10-20 23:34:22 +0800

    引入xpath处理表格 unicode转中文待解决

--- cookRecipe/spiders/xiachufang_spider.py
+++ cookRecipe/spiders/xiachufang_spider.py
@@ -17,28 +17,34 @@ class xiachufang(scrapy.Spider): #定义一个爬虫类，继承自scrapy.Spider
         #     yield scrapy.Request(url = nextUrl, callback = self.parse)
 
     def parseRecipe(self,response):
-        item = CookrecipeItem()
-        item['name'] = response.css('.page-title::text').extract_first()
-        self.writeFile('name.txt',item['name'])
-        item['image'] = response.css('.cover.image.expandable.block-negative-margin img::attr(src)').extract_first()
-        self.writeFile('image.txt',item['image'])
+        # item = CookrecipeItem()
+        # item['name'] = response.css('.page-title::text').extract_first()
+        # self.writeFile('name.txt',item['name'])
+        # item['image'] = response.css('.cover.image.expandable.block-negative-margin img::attr(src)').extract_first()
+        # self.writeFile('image.txt',item['image'])
+
         materials = []
-        table = response.css('.ings').find_all('tr')
-        print(json.dumps(table))
+        trList = response.xpath("//div[@class='ings']//table/tr")
+        for td in trList:
+            item = td.xpath("//td[@class='name']//text()").extract_first().strip().encode("utf-8").decode("utf-8")+':'+td.xpath("//td[@class='unit']//text()").extract_first().strip().encode("utf-8").decode("utf-8")
+            print(item)
+            materials.append(item)
+        self.writeFile('materials2.txt',json.dumps(materials)+'\r\n')
         # for tr in response.css('.ings tr'):
         #     materials.append('name:' + tr.css('.name::text').extract_first() + 'unit:' + tr.css('.unit::text').extract_first())
         # item['materials'] = json.dumps(materials)
         # self.writeFile('materials.txt',item['materials'])
-        steps = []
-        for step in response.css('.steps li'):
-            step = step.css('.text::text').extract_first()
-            image = step.css('img::attr(src)').extract_first()
-            if image is None:
-                iamge = ''
-            steps.append('step:' + step + 'image:'+ image )
-        item['steps'] = json.dumps(steps)
-        self.writeFile('steps.txt',item['steps'])
-        self.writeFile('test.txt',item['name'] + '---' + item['image'] + '---' + item['materials'] + '---' + item['steps'] + '\r\n')
+
+        # steps = []
+        # for step in response.css('.steps li'):
+        #     step = step.css('.text::text').extract_first()
+        #     image = step.css('img::attr(src)').extract_first()
+        #     if image is None:
+        #         iamge = ''
+        #     steps.append('step:' + step + 'image:'+ image )
+        # item['steps'] = json.dumps(steps)
+        # self.writeFile('steps.txt',item['steps'])
+        # self.writeFile('test.txt',item['name'] + '---' + item['image'] + '---' + item['materials'] + '---' + item['steps'] + '\r\n')
 
     def writeFile(self,fileName,content):
         f = open(fileName, "a+", encoding="utf-8")
